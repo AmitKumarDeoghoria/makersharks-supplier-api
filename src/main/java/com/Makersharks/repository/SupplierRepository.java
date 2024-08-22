e702d5ba15fd1ec2fc824050f65a57da27f592a2
amitkumardeoghoria <kamit5741@gmail.com>
2024-08-22 20:16:48 +0530

    repository modified a lil

--- src/main/java/com/Makersharks/repository/SupplierRepository.java
+++ src/main/java/com/Makersharks/repository/SupplierRepository.java
@@ -3,12 +3,14 @@ package com.Makersharks.repository;
 
 import com.Makersharks.model.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+public interface SupplierRepository extends JpaRepository<Supplier, Long>, CrudRepository<Supplier, Long> {
     List<Supplier> findByLocationAndNatureOfBusinessAndManufacturingProcesses(
             String location, String natureOfBusiness, String manufacturingProcesses);
+    boolean existsById(Long supplierId);
 }
